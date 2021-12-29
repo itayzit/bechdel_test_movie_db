6ae96e3087878eea6755754ad1ba69ead2e85c6f
itayzit <itay@hyro.ai>
2021-12-29 22:37:52 +0200

    write actors together with movies

--- actors.py
+++ actors.py
@@ -1,9 +1,4 @@
 import gamla
-from timeit import default_timer as timer
-
-import requests
-
-import utils
 
 ACTOR_SPEC = {
     "actor_id": gamla.itemgetter("id"),
@@ -20,38 +15,15 @@ ACTOR_SPEC = {
         ),
     ),
 }
-cnx = utils.connect_to_db()
-cursor = cnx.cursor()
-added_actors = 0
+
 stmt = (
     f"INSERT INTO actors ({', '.join(list(ACTOR_SPEC.keys()))}) " "VALUES (%s, %s, %s)"
 )
 
-for actor_id in range(5):
-    start = timer()
-    try:
-        r = requests.get(
-            "https://api.themoviedb.org/3/person/"
-            + str(actor_id)
-            + "?api_key=4dec3d85d9e68720edcce937fca2f95c"
-        )
-        if r.status_code != 200 or r.json()["known_for_department"] != "Acting":
-            continue
-        actor = gamla.pipe(
-            r,
-            lambda x: x.json(),
-            gamla.apply_spec(ACTOR_SPEC),
-        )
-        print(actor)
-        try:
-            cursor.execute(stmt, tuple(actor.values()))
-        except:
-            print("could not insert", tuple(actor.values()))
-        added_actors += 1
-        if added_actors % 100 == 0:
-            print("commiting actors till:", actor_id, "last one:", actor)
-            cnx.commit()
-            print("finished commiting")
-            print("--- %s seconds ---" % (timer() - start))
-    except Exception as e:
-        print("Error!", actor_id, "\n", e)
+
+def request_url(actor_id):
+    return (
+        "https://api.themoviedb.org/3/person/"
+        + str(actor_id)
+        + "?api_key=4dec3d85d9e68720edcce937fca2f95c"
+    )
--- write_movies_overviews_actors.py
+++ write_movies_overviews_actors.py
@@ -3,19 +3,21 @@ import gamla
 
 from timeit import default_timer as timer
 
+import actors
 import movies
 import overviews
 import utils
 
-
+problematics = set()
 cnx = utils.connect_to_db()
 cursor = cnx.cursor()
 
 start = timer()
-for movie_id in range(5, 1000):
+for movie_id in range(100, 200):
     try:
         m = requests.get(movies.request_url(movie_id))
-        if m.status_code != 200:
+        a = requests.get(actors.request_url(movie_id))
+        if m.status_code != 200 or a.status_code != 200:
             continue
         movie, overview = gamla.pipe(
             m,
@@ -26,13 +28,16 @@ for movie_id in range(5, 1000):
             ),
             tuple,
         )
+        actor = gamla.pipe(a, lambda x: x.json(), gamla.apply_spec(actors.ACTOR_SPEC))
         try:
             cursor.execute(movies.stmt, tuple(movie.values()))
             cursor.execute(overviews.stmt, tuple(overview.values()))
+            cursor.execute(actors.stmt, tuple(actor.values()))
         except Exception as e:
-            print("could not insert", movie_id, e)
-        if movie_id % 100 == 0:
-            print("commiting movies till:", movie_id, "last movie:", movie)
+            problematics.add(movie_id)
+            print("could not insert", movie_id, "error", e)
+        if movie_id % 10 == 0:
+            print("commiting till:", movie_id, "last movie:", movie)
             cnx.commit()
             print("finished commiting")
             print("--- %s seconds ---" % (timer() - start))
@@ -40,4 +45,4 @@ for movie_id in range(5, 1000):
     except Exception as e:
         print("Error!", movie_id, "\n", e)
 
-print("done!!!")
+print(f"done until {movie_id}!!!\nEncountered problems in:\n {problematics}")
